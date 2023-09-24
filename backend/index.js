4b6564824eb187b913d74ff0e77ea48ced01f783
Brandon <whoisbcox@gmail.com>
2023-09-24 02:25:53 -0500

    Adds random image placeholder to API data

--- backend/index.js
+++ backend/index.js
@@ -9,18 +9,18 @@ app.use(express.json());
 const properties = {
   count: 12,
   results: [
-    { id: 1,title: '1234 Main St' },
-    { id: 2,title: '4321 Washington Ave' },
-    { id: 3,title: '9876 Mountain St' },
-    { id: 4,title: '1122 First St' },
-    { id: 5,title: '3344 Apple Ln' },
-    { id: 6,title: '5566 Oak Blvd' },
-    { id: 7,title: '7788 Fairway Dr' },
-    { id: 8,title: '9900 Cedar Ln' },
-    { id: 9,title: '1029 Sunset Dr' },
-    { id: 10,title: '4848 Elm St' },
-    { id: 11,title: '1212 Ash St' },
-    { id: 12,title: '3434 Jackson St' },
+    { id: 1,title: '1234 Main St', background_image: 'https://picsum.photos/300/200?random=1' },
+    { id: 2,title: '4321 Washington Ave', background_image: 'https://picsum.photos/300/200?random=2' },
+    { id: 3,title: '9876 Mountain St', background_image: 'https://picsum.photos/300/200?random=3' },
+    { id: 4,title: '1122 First St', background_image: 'https://picsum.photos/300/200?random=4' },
+    { id: 5,title: '3344 Apple Ln', background_image: 'https://picsum.photos/300/200?random=5' },
+    { id: 6,title: '5566 Oak Blvd', background_image: 'https://picsum.photos/300/200?random=6' },
+    { id: 7,title: '7788 Fairway Dr', background_image: 'https://picsum.photos/300/200?random=7' },
+    { id: 8,title: '9900 Cedar Ln', background_image: 'https://picsum.photos/300/200?random=8' },
+    { id: 9,title: '1029 Sunset Dr', background_image: 'https://picsum.photos/300/200?random=9' },
+    { id: 10,title: '4848 Elm St', background_image: 'https://picsum.photos/300/200?random=10' },
+    { id: 11,title: '1212 Ash St', background_image: 'https://picsum.photos/300/200?random=11' },
+    { id: 12,title: '3434 Jackson St', background_image: 'https://picsum.photos/300/200?random=12' },
   ]
 };
 
