fa8d4ab73a364c7ebd391db974c4d5d348ab05b0
Brandon <whoisbcox@gmail.com>
2023-09-24 17:34:42 -0500

    Modifies and moves mock data to separate file

--- backend/index.js
+++ backend/index.js
@@ -2,27 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = 8080;
-
+const properties = require('./data/properties.json');
 app.use(cors());
 app.use(express.json());
-
-const properties = {
-  count: 12,
-  results: [
-    { id: 1,title: '1234 Main St', background_image: 'https://picsum.photos/300/200?random=1' },
-    { id: 2,title: '4321 Washington Ave', background_image: 'https://picsum.photos/300/200?random=2' },
-    { id: 3,title: '9876 Mountain St', background_image: 'https://picsum.photos/300/200?random=3' },
-    { id: 4,title: '1122 First St', background_image: 'https://picsum.photos/300/200?random=4' },
-    { id: 5,title: '3344 Apple Ln', background_image: 'https://picsum.photos/300/200?random=5' },
-    { id: 6,title: '5566 Oak Blvd', background_image: 'https://picsum.photos/300/200?random=6' },
-    { id: 7,title: '7788 Fairway Dr', background_image: 'https://picsum.photos/300/200?random=7' },
-    { id: 8,title: '9900 Cedar Ln', background_image: 'https://picsum.photos/300/200?random=8' },
-    { id: 9,title: '1029 Sunset Dr', background_image: 'https://picsum.photos/300/200?random=9' },
-    { id: 10,title: '4848 Elm St', background_image: 'https://picsum.photos/300/200?random=10' },
-    { id: 11,title: '1212 Ash St', background_image: 'https://picsum.photos/300/200?random=11' },
-    { id: 12,title: '3434 Jackson St', background_image: 'https://picsum.photos/300/200?random=12' },
-  ]
-};
 
 app.get('/api/properties', (req, res) => {
   res.status(200).send(properties);
