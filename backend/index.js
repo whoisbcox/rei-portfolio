42ff17ab0007dadb60c467603001f234f7c5668f
Brandon <whoisbcox@gmail.com>
2023-09-24 19:47:25 -0500

    Adds endpoints and mock data for property types

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ const cors = require('cors');
 const app = express();
 const PORT = 8080;
 const properties = require('./data/properties.json');
+const propertyTypes = require('./data/propertyTypes.json');
 app.use(cors());
 app.use(express.json());
 
+// Properties
 app.get('/api/properties', (req, res) => {
   res.status(200).send(properties);
 });
@@ -33,6 +35,36 @@ app.delete('/api/properties/:id', (req, res) => {
   const index = properties.results.indexOf(property);
   properties.results.splice(index, 1);
   res.send(properties);
+});
+
+// Property Types
+app.get('/api/property-types', (req, res) => {
+  res.status(200).send(propertyTypes);
+});
+
+app.get('/api/property-types/:id', (req, res) => {
+  const propertyType = propertyTypes.results.find(p => p.id === parseInt(req.params.id));
+  if (!propertyType) return res.status(400).send('The property type with the given ID was not found');
+  res.status(200).send(propertyType);
+});
+
+app.post('/api/property-types', (req, res) => {
+  const propertyType = {
+    id: propertyTypes.results.length + 1,
+    title: req.body.title
+  }
+
+  propertyTypes.results.push(propertyType);
+  res.send(propertyType);
+});
+
+app.delete('/api/property-types/:id', (req, res) => {
+  const propertyType = propertyTypes.results.find(p => p.id === parseInt(req.params.id));
+  if (!propertyType) return res.status(400).send('The property type with the given ID was not found');
+
+  const index = propertyTypes.results.indexOf(propertyType);
+  propertyTypes.results.splice(index, 1);
+  res.send(propertyTypes);
 });
 
 app.listen(
