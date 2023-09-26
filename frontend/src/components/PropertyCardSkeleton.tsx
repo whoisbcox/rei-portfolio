7f9dd8afdc3c14411cd42aaf0536de2435da2724
Brandon <whoisbcox@gmail.com>
2023-09-26 02:56:46 -0500

    Tweaks PropertyCardSkeleton styling

--- frontend/src/components/PropertyCardSkeleton.tsx
+++ frontend/src/components/PropertyCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Box, Skeleton, SkeletonText } from '@chakra-ui/react'
 
 const PropertyCardSkeleton = () => {
   return (
-    <Box width='300px'>
+    <Box width='100%'>
       <Skeleton height='200px' borderRadius={5} />
       <SkeletonText />
     </Box>
