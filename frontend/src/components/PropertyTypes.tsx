54eb906879a1128e6a510b829e1883017b9cbfdb
Brandon <whoisbcox@gmail.com>
2023-09-26 01:11:25 -0500

    Adds icons and loading spinner to Property types

--- frontend/src/components/PropertyTypes.tsx
+++ frontend/src/components/PropertyTypes.tsx
@@ -1,12 +1,37 @@
+import { HStack, Icon, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import usePropertyTypes from "../hooks/usePropertyTypes"
-
+import { IconType } from "react-icons";
+import { SiHomeassistantcommunitystore } from "react-icons/si"
+import { FaCampground, FaHome, FaRegBuilding, FaStethoscope, FaUmbrellaBeach } from "react-icons/fa"
+import { FaSignHanging } from "react-icons/fa6"
 
 const PropertyTypes = () => {
-  const { data } = usePropertyTypes();
+  const { data, isLoading, error } = usePropertyTypes();
+  const iconMap: {[key:number]: IconType } = {
+    1:FaHome,
+    2:FaStethoscope,
+    3:FaUmbrellaBeach,
+    4:FaSignHanging,
+    5:SiHomeassistantcommunitystore,
+    6:FaRegBuilding,
+    7:FaCampground,
+  }
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
-    <ul>
-      {data.map(propertyType => <li key={propertyType.id}>{propertyType.name}</li>)}
-    </ul>
+    <List>
+      {data.map((propertyType) => (
+        <ListItem key={propertyType.id} paddingY='10px'>
+          <HStack>
+            <Icon as={iconMap[propertyType.id]} boxSize={6} color='gray.500' />
+            <Text>
+              {propertyType.name}
+            </Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
   )
 }
 
