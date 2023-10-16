64e34b05159a2d45a3c8dae394e914b988196f36
Brandon <whoisbcox@gmail.com>
2023-10-15 20:04:21 -0500

    Refactors Property Grid

--- frontend/src/components/PropertyGrid.tsx
+++ frontend/src/components/PropertyGrid.tsx
@@ -11,15 +11,14 @@ interface Props {
 const PropertyGrid = ({propertyQuery}: Props) => {
   const {data, error, isLoading} = useProperties(propertyQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  if (error) return <Text>{error}</Text>;
   
   return (
-    <>
-    {error && <Text>{error}</Text>}
     <SimpleGrid columns={{sm: 1, md: 2, lg:3, xl:4 }} padding='10px' spacing={3}>
       {isLoading && skeletons.map(skeleton => <PropertyCardSkeleton key={skeleton} />)}
       {data.map(property => <Property key={property.id} property={property} />)}
     </SimpleGrid>
-    </>
   )
 }
 
