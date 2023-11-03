7658d576091b99cdfb968fb6c21c14ec462f5d76
Brandon <whoisbcox@gmail.com>
2023-11-03 11:45:06 -0500

    Adds admin authorization functionality

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,15 @@
+const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const { User, validate } = require('../models/user');
 const express = require('express');
 const router = express.Router();
+
+router.get('/me', auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
+});
 
 router.post('/', async (req, res) => {
   const { error } = validate(req.body);
@@ -19,6 +26,14 @@ router.post('/', async (req, res) => {
 
   const token = user.generateAuthToken();
   res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
+});
+
+router.delete('/:id', [auth, admin], async (req, res) => {
+  const user = await User.findByIdAndRemove(req.params.id);
+  
+  if (!user) return res.status(404).send('The user with the given ID could not be found.');
+  
+  res.send(user);
 });
 
 module.exports = router;
