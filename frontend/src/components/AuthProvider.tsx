2b6e48f8102f2618a3d190e1c34ffc73cda716b8
Brandon <whoisbcox@gmail.com>
2024-03-17 16:56:34 -0500

    Fixing redirect bug

--- frontend/src/components/AuthProvider.tsx
+++ frontend/src/components/AuthProvider.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { verifyToken } from '../services/verifyToken'; // Assuming you have an API function to verify tokens
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 interface AuthProviderProps {
   children: React.ReactNode;
@@ -15,25 +15,35 @@ interface AuthContextType {
 
 export const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
+const isDashboardRoute = (pathname: string) => {
+  const dashboardLayouts = ["/dashboard"];
+  return dashboardLayouts.some(layout => pathname.startsWith(layout));
+};
+
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [authState, setAuthState] = useState({ isAuthenticated: false, userId: null });
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     const checkAuth = async () => {
       const token = localStorage.getItem('jwt');
-      if (!token) return navigate('/login'); 
+      if (!token) {
+        if (isDashboardRoute(location.pathname)) return navigate('/login');
+        return;
+      } 
       try {
         const userId: string = await verifyToken(token);
         setAuthState({ isAuthenticated: true, userId });
       } catch (error) {
         console.error('Error verifying token:', error);
-        navigate('/login');
+
+        if (isDashboardRoute(location.pathname)) return navigate('/login');
       }
     };
 
     checkAuth();
-  }, [navigate]);
+  }, [location.pathname, navigate]);
 
   const login = (token: string, userId: string) => {
     localStorage.setItem('jwt', token);
